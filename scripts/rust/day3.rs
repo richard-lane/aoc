be729e1672f106387c4f62badc3f0230a8cc9483
Richard Lane <richard.lane@bristol.ac.uk>
2024-12-03 09:24:05 +0000

    do the multiplcations

--- scripts/rust/day3.rs
+++ scripts/rust/day3.rs
@@ -10,11 +10,12 @@ fn main() -> io::Result<()> {
 
     // Record which indices correspond to the mul( characters
     let mul_indices = find_muls(&contents);
-    println!("{:?}", mul_indices);
 
-    // Find out which ones are valid
-    // perform multiplications
-    // Add the results
+    let mut result = 0;
+    for index in mul_indices {
+        result += mul_numbers(&contents, index);
+    }
+    println!("Result: {}", result);
 
     Ok(())
 }
@@ -39,4 +40,48 @@ fn find_muls(s: &str) -> Vec<usize> {
         }
     }
     mul_indices
+}
+
+// Starting at a given index, find the numbers that need to be multiplied
+// and multiply them
+// If the numbers are not valid, return 0
+fn mul_numbers(s: &str, index: usize) -> i32 {
+    let mut n1 = Vec::new();
+    let mut n2 = Vec::new();
+    let mut n1_done = false;
+
+    for i in index + 4..index + 12 {
+        // Check if it is a digit
+        let c = s.chars().nth(i).unwrap();
+        if c.is_digit(10) {
+            // If we're not done with the first number, add it to n1
+            if !n1_done {
+                n1.push(s.chars().nth(i).unwrap());
+            } else {
+                n2.push(s.chars().nth(i).unwrap());
+            }
+        } else if c == ',' {
+            if !n1_done {
+                n1_done = true;
+            } else {
+                // Second comma encountered - invalid mul
+                return 0;
+            }
+        } else if c == ')' {
+            // Check if we have valid numbers
+            if n1.len() == 0 || n2.len() == 0 {
+                return 0;
+            }
+
+            // We're done - turn the vectors into numbers and parse them
+            let n1 = n1.into_iter().collect::<String>().parse::<i32>().unwrap();
+            let n2 = n2.into_iter().collect::<String>().parse::<i32>().unwrap();
+            return n1 * n2;
+        } else {
+            // Invalid character
+            return 0;
+        }
+    }
+    // We didn't find a close bracket
+    0
 }
