f9799f9633cc52b40768e9b09c2a3388a9643555
Richard Lane <richard.lane@bristol.ac.uk>
2024-12-06 10:56:29 +0000

    fcn for initial conditions

--- scripts/rust/day6.rs
+++ scripts/rust/day6.rs
@@ -5,35 +5,13 @@ fn main() {
     let input = read_input(path);
 
     // Find the co-ordinate where the guard starts
-    let my_dict: HashMap<char, (i32, i32)> =
+    let dirn_map: HashMap<char, (i32, i32)> =
         [('>', (0, 1)), ('<', (0, -1)), ('^', (-1, 0)), ('v', (1, 0))]
             .iter()
             .cloned()
             .collect();
 
-    // Find the direction she is moving in and her location
-    let mut found = false;
-    let mut loc = (0, 0);
-    let mut direction = (0, 0);
-    for (i, row) in input.iter().enumerate() {
-        for (j, col) in row.iter().enumerate() {
-            if found {
-                break;
-            };
-            if my_dict.contains_key(col) {
-                loc = (i as i32, j as i32);
-                direction = my_dict.get(col).unwrap().clone();
-                found = true;
-            }
-        }
-        if found {
-            break;
-        };
-    }
-    // If we didn't break, raise an error
-    if !found {
-        panic!("Guard not found");
-    }
+    let (mut loc, mut direction) = initial_conditions(&input, &dirn_map);
 
     // Initialise an array storing if the guard has visited a co-ordinate
     let mut visited = vec![vec!['.'; input[0].len()]; input.len()];
@@ -68,12 +46,47 @@ fn main() {
         } else {
             loc = next_loc;
         };
+
+        // Find out if we have visited this co-ordinate before and are facing the same direction
     }
 
     // Count the number of visited co-ordinates
     // THis is the number of Xs
     let count = visited.iter().flatten().filter(|&&x| x == 'X').count();
     println!("Number of houses visited: {}", count);
+}
+
+// Find the initial location and direction
+fn initial_conditions(
+    input: &Vec<Vec<char>>,
+    dirn_map: &HashMap<char, (i32, i32)>,
+) -> ((i32, i32), (i32, i32)) {
+    // Find the direction she is moving in and her location
+    let mut found = false;
+    let mut loc = (0, 0);
+    let mut direction = (0, 0);
+
+    for (i, row) in input.iter().enumerate() {
+        for (j, col) in row.iter().enumerate() {
+            if found {
+                break;
+            };
+            if dirn_map.contains_key(col) {
+                loc = (i as i32, j as i32);
+                direction = dirn_map.get(col).unwrap().clone();
+                found = true;
+            }
+        }
+        if found {
+            break;
+        };
+    }
+    // If we didn't break, raise an error
+    if !found {
+        panic!("Guard not found");
+    }
+
+    (loc, direction)
 }
 
 // Read the text file into a vector of vector of chars
