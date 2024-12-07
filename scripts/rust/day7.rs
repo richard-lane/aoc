edd454bb528cf0d2e5a51c27ebd302ce06094fb0
Richard Lane <richard.lane@bristol.ac.uk>
2024-12-07 18:43:45 +0000

    operator combinations

--- scripts/rust/day7.rs
+++ scripts/rust/day7.rs
@@ -1,3 +1,11 @@
+//! ```cargo
+//! [dependencies]
+//! itertools = "0.10"
+//! ```
+
+use itertools::Itertools;
+use std::collections::HashSet;
+
 fn main() {
     // Possible operators
     let operators = ['+', '*'];
@@ -8,8 +16,6 @@ fn main() {
     let lines = std::fs::read_to_string(path).unwrap();
 
     for line in lines.lines() {
-        let mut total = 0;
-
         // Split by space
         let all_numbers: Vec<&str> = line.split_whitespace().collect();
 
@@ -19,15 +25,37 @@ fn main() {
         let target_num = target_str.as_str().parse::<i32>().unwrap();
 
         // Make a vector of numbers
-        let numbers = all_numbers[1..].iter().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+        let numbers = all_numbers[1..]
+            .iter()
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
 
         // Find how many operators we need
         let n_operators = numbers.len() - 1;
 
         // Make every possible permutation of operators
-        let operator_lists = Vec<Vec<char>>::new();
+        let operator_combinations: HashSet<Vec<char>> = operators
+            .iter()
+            .cloned()
+            .combinations_with_replacement(n_operators) // Combinations allow repetition
+            .flat_map(|combo| combo.into_iter().permutations(n_operators)) // Get permutations
+            .map(|perm| perm.into_iter().collect::<Vec<char>>()) // Convert permutations to Vec<char>
+            .collect();
 
         // For each permutation, calculate the total
+        for operator_combo in operator_combinations {
+            // Calculate the total based on this operator combination
+            println!("{:?}", operator_combo);
+
+            // let mut result = numbers[0];
+            // for (i, &op) in operator_combo.iter().enumerate() {
+            //     match op {
+            //         '+' => result += numbers[i + 1],
+            //         '*' => result *= numbers[i + 1],
+            //         _ => panic!("Unexpected operator"),
+            //     }
+            // }
+        }
         // Check if this is correct
         // If it is, incrememt the counter
     }
