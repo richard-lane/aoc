1cda7b8f65e076fea5bf457f7705b5561f3da7a0
Richard Lane <richard.lane@bristol.ac.uk>
2024-12-07 18:15:56 +0000

    read the other numbers

--- scripts/rust/day7.rs
+++ scripts/rust/day7.rs
@@ -9,18 +9,20 @@ fn main() {
 
     for line in lines.lines() {
         // Split by space
-        let numbers: Vec<&str> = line.split_whitespace().collect();
+        let all_numbers: Vec<&str> = line.split_whitespace().collect();
 
         // The total is the first one, with the colon removed
-        let mut target_str = numbers[0].chars();
+        let mut target_str = all_numbers[0].chars();
         assert!(target_str.next_back().unwrap() == ':');
         let target_num = target_str.as_str().parse::<i32>().unwrap();
-        println!("{:?}", target_num)
+
+        // Make a vector of numbers
+        let numbers = all_numbers[1..].iter().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+
+        // Find how many operators we need
+        // Make every possible permutation of operators
+        // For each permutation, calculate the total
+        // Check if this is correct
+        // If it is, incrememt the counter
     }
-    // Make a vector of numbers
-    // Find how many operators we need
-    // Make every possible permutation of operators
-    // For each permutation, calculate the total
-    // Check if this is correct
-    // If it is, incrememt the counter
 }
