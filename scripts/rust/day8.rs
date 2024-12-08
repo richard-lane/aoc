1853462a1cff43a68c40b7422bef1d8667d42c26
Richard Lane <richard.lane>
2024-12-08 09:21:36 +0000

    read file

--- scripts/rust/day8.rs
+++ scripts/rust/day8.rs
@@ -1,8 +1,21 @@
-fn main {
+use std::fs;
+
+fn main () {
     // Parse the input map
+    let input = parse_input("inputs/day8.txt");
+
     // Find antenna positions
     // Find pairs with matching frequencies
     // Find antinode locations
     // Handle overlaps
     // Count unique locations
+}
+
+fn parse_input(input: &str) -> Vec<Vec<char>> {
+    fs::read_to_string(input).expect("Error reading file")
+        .lines()
+        .map(|line| {
+            line.chars().collect::<Vec<char>>()
+        })
+        .collect::<Vec<Vec<char>>>()
 }
