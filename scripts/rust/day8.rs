6cfce7313b31735dba595350d65de179e4506753
Richard Lane <richard.lane>
2024-12-08 18:33:48 +0000

    build hashmap

--- scripts/rust/day8.rs
+++ scripts/rust/day8.rs
@@ -1,18 +1,33 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn main () {
     // Parse the input map
     let input = parse_input("inputs/day8.txt");
 
     // Find antenna positions
+    let mut freq_groups: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
+    for r in 0..input.len() {
+        for c in 0..input[0].len() {
+            if input[r][c] != '.' {
+                freq_groups
+                    .entry(input[r][c])
+                    .or_insert_with(Vec::new)
+                    .push((r, c));
+            }
+        }
+    }
+    println!("{:?}", freq_groups);
+    
     // Find pairs with matching frequencies
-    // Find antinode locations
+    // Find antinode locations by taking the distance between them and adding the distance to the first antinode
     // Handle overlaps
     // Count unique locations
 }
 
 fn parse_input(input: &str) -> Vec<Vec<char>> {
-    fs::read_to_string(input).expect("Error reading file")
+    fs::read_to_string(input)
+        .expect("Error reading file")
         .lines()
         .map(|line| {
             line.chars().collect::<Vec<char>>()
