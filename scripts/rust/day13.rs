65647c5607076c59e53010f6674be17fd106fa44
Richard Lane <richard.lane@bristol.ac.uk>
2024-12-13 14:19:11 +0000

    read into matrix

--- scripts/rust/day13.rs
+++ scripts/rust/day13.rs
@@ -7,37 +7,45 @@ fn main() -> io::Result<()> {
     let _ = File::open(path)?.read_to_string(&mut input);
 
     for machine in input.split("\n\n").collect::<Vec<&str>>() {
-        println!("{}\n", machine);
-    }
-    //     let line = line?;
-    //     if line.starts_with("Button A:") || line.starts_with("Button B:") {
-    //         let parts: Vec<&str> = line.split(", ").collect();
-    //         let x_part = parts[0].split(": ").collect::<Vec<&str>>()[1];
-    //         let y_part = parts[1];
-    //         let x_value: i32 = x_part[2..].parse().unwrap();
-    //         let y_value: i32 = y_part[2..].parse().unwrap();
-    //         matrix[0].push(x_value);
-    //         matrix[1].push(y_value);
-    //     } else if line.starts_with("Prize:") {
-    //         let parts: Vec<&str> = line.split(", ").collect();
-    //         let x_part = parts[0].split("=").collect::<Vec<&str>>()[1];
-    //         let y_part = parts[1].split("=").collect::<Vec<&str>>()[1];
-    //         let x_value: i32 = x_part.parse().unwrap();
-    //         let y_value: i32 = y_part.parse().unwrap();
-    //         prize.push(x_value);
-    //         prize.push(y_value);
-    //     }
-    // }
+        let (matrix, prize) = parse(machine);
 
-    // println!("Matrix: {:?}", matrix);
-    // println!("Prize: {:?}", prize);
+        println!("Matrix: {:?}", matrix);
+        println!("Prize: {:?}", prize);
+    }
 
     Ok(())
 }
 
 // Helper function to parse the input into a matrix and vector
 fn parse(input: &str) -> (Vec<Vec<i32>>, Vec<i32>) {
-    println!("{:?}", input);
+    // Init an empty 2x2 matrix
+    let mut matrix = vec![vec![0; 2]; 2];
+    let mut prize = vec![0; 2];
 
-    (Vec::new(), Vec::new())
+    let split_input = input.split("\n").collect::<Vec<&str>>();
+    assert_eq!(split_input.len(), 3);
+
+    // First line is A
+    for i in 0..2 {
+        let x = parse_line(split_input[i]);
+        matrix[i][0] = x.0;
+        matrix[i][1] = x.1;
+    }
+
+    // Third line is 3
+    let x = parse_line(split_input[2]);
+    prize[0] = x.0;
+    prize[1] = x.1;
+
+    (matrix, prize)
+}
+
+// Get the button coordinates
+fn parse_line(input: &str) -> (i32, i32) {
+    let parts: Vec<&str> = input.split(", ").collect();
+    let x_part = parts[0].split(": ").collect::<Vec<&str>>()[1];
+    let y_part = parts[1];
+    let x_value: i32 = x_part[2..].parse().unwrap();
+    let y_value: i32 = y_part[2..].parse().unwrap();
+    (x_value, y_value)
 }
